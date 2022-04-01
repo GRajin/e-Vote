54f7d05ecf3320d7db2a9aaae87a27c7ec6ac8a0
Rajin Gangadharan <rajin.gangadharan@gmail.com>
2022-04-01 18:58:03 +0530

    Fixes in register screen

--- app/src/main/java/com/ionic/eVote/RegisterActivity.kt
+++ app/src/main/java/com/ionic/eVote/RegisterActivity.kt
@@ -1,15 +1,21 @@
-package com.ionic.eVote;
+package com.ionic.eVote
 
-import android.os.Bundle;
+import android.os.Bundle
+import android.view.View
+import android.widget.ArrayAdapter
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatAutoCompleteTextView
+import com.ionic.eVote.databinding.ActivityRegisterBinding
 
-import androidx.appcompat.app.AppCompatActivity;
-
-public class RegisterActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
-
+class RegisterActivity : AppCompatActivity() {
+    private var binding: ActivityRegisterBinding? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        val view: View = binding!!.root
+        setContentView(view)
+        val arrGender = listOf("Male", "Female", "Other")
+        val adapter = ArrayAdapter(this, R.layout.list_gender, arrGender)
+        ((binding!!.layGender.editText) as AppCompatAutoCompleteTextView).setAdapter(adapter)
     }
 }
